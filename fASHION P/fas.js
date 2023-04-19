c822f362d3cf4e3a44c6c710afa7e2841266cdde
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-04-19 09:22:52 +0300

    A-A-A

--- fASHION P/fas.js
+++ fASHION P/fas.js
@@ -1,5 +1,5 @@
 console.log("Jonas Class 101")
-
+//Fundamental Part 1//
 //linking JavaScript file Day 101 //
 let JavaScript = 'amazing';
 if (JavaScript === 'amazing' )alert("JavaScript is FUN");
