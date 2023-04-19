fc19e0985076a7b34173a881a820888b1fff4894
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-04-19 15:11:11 +0300

    Variable

--- fASHION P/fas.js
+++ fASHION P/fas.js
@@ -6,4 +6,21 @@ if (JavaScript === 'amazing' )alert("JavaScript is FUN");
 
 //Value (basically a Piece of data smallest unit of information on JS) AND variables//
 
-//e.g 'San Wu' is a Value //
+//e.g 'San Wu' is a Value // // variables should be written in camel case or first_name (underscore)//
+
+let firstName = "Yang Wu"
+console.log(firstName); //should be able to print Yang Wu
+console.log(firstName); //should be able to print Yang Wu
+console.log(firstName); //should be able to print Yang Wu
+//variable in underscore//
+let last_name = "Ming Ya"
+console.log(last_name)
+//syntax Error = error in writting code i.e on variables i.e starting a variable name with a number//
+//UNDERSTANDABLE NAMING OF VARIABLES//
+let myFirstJob= 'Grave Digger';
+let myCurrentJob = 'Senior Software Engineer';
+//bad naming variables//
+let job1 = 'gravedigger';
+let job2 = 'SWE II';
+
+console.log(myFirstJob); //print Grave Digger 
