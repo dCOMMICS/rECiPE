853a3a7b775f250e0a1cb9af83f6c93e1f00865f
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-04-19 11:03:25 +0300

    1

--- fASHION P/fas.js
+++ fASHION P/fas.js
@@ -3,3 +3,7 @@ console.log("Jonas Class 101")
 //linking JavaScript file Day 101 //
 let JavaScript = 'amazing';
 if (JavaScript === 'amazing' )alert("JavaScript is FUN");
+
+//Value (basically a Piece of data smallest unit of information on JS) AND variables//
+
+//e.g 'San Wu' is a Value //
