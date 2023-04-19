031392e3d3ee3ee10fd7ad29e1017eac82f166a5
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-04-19 15:51:28 +0300

    dAY 1 C

---
+++ fASHION P/PY/day.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!");
+   printf("aM High AF");
+  return 0;
+}
+
+// this is a comment//
+// #include <stdio.h> this is the header file binary on c language that let us work with input and output on code.
+/* 
+printf() adds functionality to C programs
+any code inside the curly brackets will be executed DONT FORGET TO CLOSE THE CURLY BRAKETS.
+OTHER WAY OF WRITTING THE PROGRAM
+int main(){printf("Hello World!");return 0;}
+*/
